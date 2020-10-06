f75c1abd61d3c38c296cfaba471b36155bf2c136
Björn Hommel <bjorn.hommel@gmail.com>
2020-10-06 11:41:51 +0200

    init commit

--- R/max_similarity.R
+++ R/max_similarity.R
@@ -5,11 +5,7 @@
 #' @param description
 #' @return description
 #' @export
-similarity_coefficient <- function(similarity_mtx, x_axis = TRUE, threshold = NULL, echo = TRUE) {
-
-  if (!is.null(threshold) & !is.numeric(threshold)) {
-    stop('`threshold` needs to be NULL or numeric!')
-  }
+max_similarity <- function(similarity_mtx, x_axis = TRUE, echo = TRUE) {
 
   margin <- if (x_axis) 1 else 2
 
@@ -18,25 +14,28 @@ similarity_coefficient <- function(similarity_mtx, x_axis = TRUE, threshold = NU
   if (check_obj(similarity_mtx)) {
 
     # find maximum similarity from matrix
-    if (echo) cat('Calculating similarity coefficients...\n')
+    if (echo) cat('Finding most similar indicies...\n')
     export <- apply(
       X = similarity_mtx,
       MARGIN = margin,
       FUN = function(similarities) {
+
         valid_similarities <- na.omit(similarities)
-        max_similarity <- if (length(valid_similarities) < 1) NA else max(similarities, na.rm = TRUE)
+
+        if (length(valid_similarities) < 1) {
+          return(NA)
+        } else if (length(which.max(valid_similarities)) < 1){
+          return(NA)
+        } else {
+          return(which.max(similarities))
+        }
       }
     )
 
-    # coerce into dichotomous similarity score if threshold is defined
-    if (!is.null(threshold)) {
-      export <- dplyr::if_else(export >= threshold, 1, 0)
-    }
+    return(export)
 
   } else {
     stop('`similarity_mtx` must be matrix from `clis::similarity_matrix()`')
   }
-
-  return(export)
 
 }
